a1c1f8519e100fa2dc56147128224671f0d9195d
Surya Merlin Jose <85429137+SuryaMerlinJose@users.noreply.github.com>
2024-03-19 10:44:07 +0530

    19-03-2024

--- Zip and Unzip Folder.py
+++ Zip and Unzip Folder.py
@@ -2,7 +2,7 @@ import os
 import shutil
 import  zipfile
 
-#-----------Required paths are defined here ------------------------------------
+#-----------Required paths are defined here -------------------------------------------------------
 
 download_path = r"C:\Users\Surya\Downloads\2024-02-28.zip"
 DXC_SP = r"C:\Task_Zip\DXC_SP"
@@ -11,7 +11,7 @@ Processed_path = r"C:\Task_Zip\Processed"
 #name of the zipped folder
 folder_name="2024-02-28.zip" 
 
-#---------- Function for moving and unzipping folder----------------------------
+#---------- Function for moving and unzipping folder------------------------------------------------
 def DXC_SP_File_Move():
     Len_DXC = len(os.listdir(DXC_SP))
     list_dxc = os.listdir(DXC_SP)  # processed data in dxc
@@ -26,17 +26,47 @@ def DXC_SP_File_Move():
         for file_name in list_dxc:
             source = os.path.join(DXC_SP, file_name)
             destination = os.path.join(Processed_path, file_name)
-            shutil.move(source, destination)
+            shutil.move(source, destination)  
         #moving zip file to DXC_SP    
         move_and_unzip()
 
-#------------moving the zip from downloads to DXC_SP--------------------------
+#------------moving the zip from downloads to DXC_SP--------------------------------------------------
         
 def move_and_unzip():
         shutil.move(download_path, DXC_SP)
         with zipfile.ZipFile(os.path.join(DXC_SP, folder_name), 'r') as zip_ref:
             zip_ref.extractall(DXC_SP)
         os.remove(os.path.join(DXC_SP, folder_name))
+
+#--------------Code to take the excel outside the folder-----------------------------------------------
+
+        for root, dirs, files in os.walk(DXC_SP):
+            for file in files:
+                if file.endswith(".xlsx"):
+                    source_path = os.path.join(root, file)
+                    shutil.move(source_path,DXC_SP)
+                    print(os.path.join(root, file))
+
+#---------------Code to move the empty folder to processed folder----------------------------------------
+
+        list_dxc_2 = os.listdir(DXC_SP)
+        print(list_dxc_2)
+        for fi in list_dxc_2:
+             file_path=os.path.join(DXC_SP, fi)
+             if ".xlsx" not in fi:
+                folder=os.path.join(DXC_SP, fi)
+                dest=os.path.join(Processed_path, fi)
+                shutil.move(folder,dest)
+                """
+                ERROR!
+                os.remove(os.path.join(DXC_SP, fi))
+                PermissionError: [WinError 5] Access is denied: 'C:\\Task_Zip\\DXC_SP\\2024-02-28'
+                """
+
+                       
+
+                      
+        
 
 #--------------call the main function-------------------
         
@@ -48,5 +78,12 @@ DXC_SP_File_Move()
 """ 
 zipfile.ZipFile: This is a class from Python's zipfile module used to work with ZIP archives.
 
+"""
+
+"""
+ERROR!
+
+os.remove(os.path.join(DXC_SP, fi))
+PermissionError: [WinError 5] Access is denied: 'C:\\Task_Zip\\DXC_SP\\2024-02-28'
 """
     
